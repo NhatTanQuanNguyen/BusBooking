e38de734a3766e3ccb51e006df82a32ee8c9ee8c
Your Name <you@example.com>
2026-01-19 17:29:36 +0700

    CI/CD config

---
+++ Backend/src/v1/services/cache.service.js
@@ -0,0 +1,65 @@
+const {Cache} = require('../databases/redis/init.ioredis')
+
+class CacheService{
+    /**
+     * @param {import('ioredis').Redis} cache
+    */
+    constructor(cache){
+        this.cache = cache
+    }
+
+    async setCache({key,value}){
+        throw new Error('setCache() must be implemented')
+    }
+
+    async setCacheTTL({key,value,ttl}){
+        throw new Error('setCacheTTL() must be implemented')
+    }
+
+    async getCache({key}){
+        throw new Error('getCache() must be implemented')
+    }
+
+    async deleteCache({key}){
+        throw new Error('deleteCache() must be implemented')
+    }
+    
+}
+
+
+class RedisCacheService extends CacheService{
+
+    constructor(cache){
+        super(cache)
+        
+    }
+
+    async setCache({key,value}){
+        const data = typeof value === 'string'
+                    ?value
+                    : JSON.stringify(value)
+        return this.cache.set(key,data)
+    }
+
+    async setCacheTTL({key,value,ttl}){
+        const data = typeof value === 'string'
+                    ?value
+                    : JSON.stringify(value)
+        return this.cache.set(key,data,'EX',ttl)
+    }
+
+    async getCache({key}){
+        const data = await this.cache.get(key)
+        if (!data) return null;
+        return JSON.parse(data)
+    }
+
+    async deleteCache({key}){
+        return this.cache.del(key)
+    }
+
+}
+
+module.exports = {
+    redisCacheService : new RedisCacheService(Cache.getInstance())
+}
