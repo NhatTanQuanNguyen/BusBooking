b30db0d533561689bb09d1880bdfbd76d87a2b0f
Your Name <you@example.com>
2026-01-19 13:23:57 +0700

    feature(cache): completed (#10)

---
+++ Backend/src/v1/core/error.response.js
@@ -0,0 +1,71 @@
+class ErrorResponse extends Error {
+    constructor({
+        statusCode = 500,
+        message = 'Internal Server Error',
+        errors = null
+    }) {
+        super(message)
+
+        this.status = 'error'
+        this.statusCode = statusCode
+        this.message = message
+        if (errors) this.errors = errors
+
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class BadRequestError extends ErrorResponse {
+    constructor({ message = 'Bad Request', errors = null }) {
+        super({
+            statusCode: 400,
+            message,
+            errors
+        })
+    }
+}
+
+
+class UnauthorizedError extends ErrorResponse {
+    constructor({ message = 'Unauthorized' }) {
+        super({
+            statusCode: 401,
+            message
+        })
+    }
+}
+
+class ForbiddenError extends ErrorResponse {
+    constructor({ message = 'Forbidden' }) {
+        super({
+            statusCode: 403,
+            message
+        })
+    }
+}
+
+
+class NotFoundError extends ErrorResponse {
+    constructor({ message = 'Not Found' }) {
+        super({
+            statusCode: 404,
+            message
+        })
+    }
+}
+
+class Conflict extends ErrorResponse {
+    constructor({ message = 'Conflict' }) {
+        super({
+            statusCode: 409,
+            message
+        })
+    }
+}
+module.exports = {
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    Conflict
+}
--- Backend/src/v1/routes/index.js
+++ Backend/src/v1/routes/index.js
@@ -1,12 +1,11 @@
+const { helloController } = require('../controllers');
+const { asyncHandler } = require('../helpers/handler/asyncHandler');
 const { logger } = require('../helpers/logger/myLogger');
 const { generateRequestId } = require('../middleware');
 
 const router = require('express').Router()
 
 
-router.get("/hello",generateRequestId,(req,res,next) => {
-    logger.info("ConCac",req.requestId,{"name" : "Thanh Tan"})
-    res.send(JSON.stringify({"name" : "Thanh Tan"}))
-})
+router.get("/hello",generateRequestId,asyncHandler(helloController))
 
 module.exports = router;
